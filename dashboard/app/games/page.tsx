01a4f4e6ebcae0f6bc1321d317e061fda657a111
AlexSportii <alejandro@sportii.app>
2026-01-28 23:59:32 +0200

    feat: Implement a new games page using a dedicated client component.

---
+++ dashboard/app/games/page.tsx
@@ -0,0 +1,10 @@
+import GamesPageClient from "../components/GamesPageClient";
+
+export const metadata = {
+    title: 'Sportii - All Games',
+    description: 'Find a game near you.',
+};
+
+export default function Page() {
+    return <GamesPageClient />;
+}
